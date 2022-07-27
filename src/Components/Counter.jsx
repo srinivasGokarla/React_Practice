59571fe1d5de1c6e249d76b29c22f1c3c7ca75af
SrinivasGokarla <srinivasgokarla@gmail.com>
2022-07-27 21:14:39 +0530

    counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,13 +2,24 @@ import React, {useState} from 'react'
 
 export default function Counter() {
     const[count, setCount] = useState(0)
+  
+    const Increment = () => {
+      setCount(count+1)
+    }
+    const Decrement = () => {
+      if(count > 0) {
+        setCount(count-1)
+      } else {
+        setCount(0)
+      }
+    }
 
   return (
     <div>
         <h2>Counter Problem</h2>
         <h4>Counter: {count}</h4>
-        <button onClick={() => setCount(count+1)}>+</button>
-        <button onClick={() => setCount(count-1)}>-</button>
+        <button onClick={ Increment }>+</button>
+        <button onClick={Decrement}>-</button>
 
     </div>
    
