fbbc88192ec2fbf070cc54d9f3ff5204f0cdbe37
srinivasGokarla <srinivasgokarla@gmail.com>
2022-09-18 20:14:14 +0530

    pop

---
+++ src/Components/pop.js
@@ -0,0 +1,11 @@
+const firstArray=[1,2,"srinu", "madhu","padma"];
+console.log(firstArray.pop());
+
+let firstmadhu=[1,2,3,5,6,"madhu","srinu","9"];
+console.log(firstmadhu.pop());
+
+console.log(firstmadhu[4]);
+
+
+const numbers= [10,20,30,40,50];
+console.log(numbers.reduce((x,y)=> x+y,4) );
