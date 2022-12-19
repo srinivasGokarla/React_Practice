de3ef1bd3e8ab46a6bb473090607eb20649f298a
srinivasGokarla <srinivasgokarla@gmail.com>
2022-12-19 17:21:04 +0530

    react

--- src/Components/Usecontext.jsx
+++ src/Components/Usecontext.jsx
@@ -1,39 +1,47 @@
-import  React,  { useContext } from 'react';
-import ReactDOM from "react-dom";
+import React, { useState, useContext } from "react";
+import "./styles.css";
+
 const themes = {
-    light: {
-      foreground: "#000000",
-      background: "#eeeeee"
-    },
-    dark: {
-      foreground: "#ffffff",
-      background: "#222222"
-    }
-  };
-  
-  const ThemeContext = React.createContext(themes.light);
-  
- export default function Usecontext() {
-    return (
-      <ThemeContext.Provider value={themes.dark}>
-        <Toolbar />
-      </ThemeContext.Provider>
-    );
+  light: {
+    color: "black",
+    background: "white"
+  },
+  dark: {
+    color: "white",
+    background: "black"
   }
-  
-  function Toolbar(props) {
-    return (
-      <div>
-        <ThemedButton />
-      </div>
-    );
-  }
-  
-  function ThemedButton() {
-    const theme = useContext(ThemeContext);
-    return (
-      <button style={{ background: theme.background, color: theme.foreground }}>
-        I am styled by theme context!
-      </button>
-    );
-  }
+};
+
+const ThemeContext = React.createContext(themes.dark);
+
+function Comp1() {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div style={{ backgroundColor: theme.background, color: theme.color }}>
+      <h1>Current Theme: {theme.color}</h1>
+    </div>
+  );
+}
+
+function Comp2() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div>
+      <button onClick={() => setTheme(themes.light)}>Light</button>
+      <button onClick={() => setTheme(themes.dark)}>Dark</button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }} className="App">
+      <Comp1 />
+      <Comp2 />
+    </ThemeContext.Provider>
+  );
+}
